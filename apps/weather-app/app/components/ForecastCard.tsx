aa632257d235334a2b5d9ed1254bb66781d4a366
Linden Holt-Whittaker <36598942+LindenHolt-Whittaker@users.noreply.github.com>
2025-11-24 19:59:04 +0000

    Added formatting to conditions in ForecastCard component

--- apps/weather-app/app/components/ForecastCard.tsx
+++ apps/weather-app/app/components/ForecastCard.tsx
@@ -42,6 +42,23 @@ export default function ForecastCard({
     }`;
   };
 
+  // Split string conditions by comma and show each condition on new line.
+  // Most conditions show two conditions, so if only one, add breaking space for UI consistency.
+  const getFormattedConditions = (conditions: string) => {
+    const nonBreakingSpace = "\u00A0";
+
+    const formattedConditions = conditions
+      .split(",")
+      .map((condition, i) => <div key={condition}>{condition}</div>);
+
+    if (formattedConditions.length === 1)
+    {
+      formattedConditions.push(<div>{nonBreakingSpace}</div>)
+    }
+
+    return formattedConditions;
+  };
+
   return (
     <div className="forecast-card">
       <div className="forecast-day">
@@ -50,7 +67,9 @@ export default function ForecastCard({
       <div className="forecast-weather-icon-container">
         <WeatherIcon icon={forecast.icon} />
       </div>
-      <div className="forecast-conditions">{forecast.conditions}</div>
+      <div className="forecast-conditions">
+        {getFormattedConditions(forecast.conditions)}
+      </div>
       <div className="forecast-temps">
         <span className="temp-high">
           {Math.round(maxTemp)}°{unit}
